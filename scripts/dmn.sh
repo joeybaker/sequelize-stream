b24cf87f2f06c4c7e192b7a6f8f81af758fbeb7f
Joey Baker <joey@byjoeybaker.com>
2016-09-24 16:22:52 -0700

    Chore install dmn

---
+++ scripts/dmn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+function commit_npmignore () {
+  if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != '' ]]; then
+    git add .npmignore && \
+    git commit --no-verify -m'update npmignore'
+  fi
+}
+
+dmn gen -f . && commit_npmignore
+
